eb529a929fe9b78c438ebf6cf63649d0e0525923
evilDAVE <aymanfarsi99@gmail.com>
2023-12-25 22:53:25 -0500

    Update Windows subsystem configuration

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
-#![windows_subsystem = "windows"]
+#![warn(clippy::all, rust_2018_idioms)]
+#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")] // hide console window on Windows in release
 
 use std::{
     env,
