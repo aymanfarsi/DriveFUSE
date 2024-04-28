bf12f660f69d5ef9d86efaa079c7624e28b7bd37
evilDAVE <aymanfarsi99@gmail.com>
2024-04-28 16:11:22 -0400

    Right-click on number of mounted storages to mount and unmount all

--- src/backend/mounting.rs
+++ src/backend/mounting.rs
@@ -32,7 +32,7 @@ impl MountingStorage {
     }
 
     pub fn is_drive_letter_mounted(&self, drive: char) -> bool {
-        self.mounted.values().any(|&v| v == drive)
+        self.mounted.values().any(|&v| v == drive) || available_drives().contains(&drive)
     }
 
     pub fn is_mounted(&self, name: String) -> bool {
--- src/ui/top_panel.rs
+++ src/ui/top_panel.rs
@@ -1,6 +1,9 @@
 use egui::{menu, Align, Context, Layout, RichText, TextStyle, TopBottomPanel};
 
-use crate::{utilities::enums::Tab, RcloneApp};
+use crate::{
+    utilities::enums::{Message, Tab},
+    RcloneApp,
+};
 
 pub fn render_top_panel(ctx: &Context, app: &mut RcloneApp) {
     TopBottomPanel::top("top_panel").show(ctx, |ui| {
@@ -9,11 +12,23 @@ pub fn render_top_panel(ctx: &Context, app: &mut RcloneApp) {
             ui.visuals_mut().button_frame = false;
             menu::bar(ui, |ui| {
                 ui.with_layout(Layout::left_to_right(Align::Min), |ui| {
-                    ui.label(
+                    let label = ui.label(
                         RichText::new(app.mounted_storages.total_mounted().to_string())
                             .size(14.)
                             .strong(),
                     );
+                    label.context_menu(|ui| {
+                        if ui.button("Mount all").clicked() {
+                            app.mounted_storages.mount_all(app.rclone.storages.clone());
+                        }
+                        if ui.button("Unmount all").clicked() {
+                            app.mounted_storages.unmount_all();
+                        }
+                        ui.separator();
+                        if ui.button("Quit").clicked() {
+                            app.tx_egui.send(Message::Quit).unwrap();
+                        }
+                    });
                     ui.add_space(3.);
                     for tab in Tab::values() {
                         let btn = ui.selectable_label(
